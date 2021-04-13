0995a76ed7d305fea40b08153546801331af99c9
hotyute <hotyute_mason@hotmail.com>
2021-04-13 03:21:04 +0100

    combineCallBack

--- ASDE_X/renderer.cpp
+++ ASDE_X/renderer.cpp
@@ -1039,18 +1039,15 @@ void CALLBACK combineCallback(GLdouble coords[3],
 	GLdouble* vertex_data[4],
 	GLfloat weight[4], GLdouble** dataOut)
 {
-	GLdouble* vertex;
-	int i;
-
-	vertex = (GLdouble*)malloc(6 * sizeof(GLdouble));
+	GLdouble* vertex = new GLdouble[6];
 	vertex[0] = coords[0];
 	vertex[1] = coords[1];
 	vertex[2] = coords[2];
-	for (i = 2; i < 6; i++) {
+	for (int i = 3; i < 6; i++) {
 		vertex[i] = weight[0] * vertex_data[0][i]
 			+ weight[1] * vertex_data[1][i];
-		+weight[2] * vertex_data[2][i];
-		+weight[3] * vertex_data[3][i];
+			+ weight[2] * vertex_data[2][i];
+			+ weight[3] * vertex_data[3][i];
 	}
 	*dataOut = vertex;
 	tesses.push_back(vertex);
